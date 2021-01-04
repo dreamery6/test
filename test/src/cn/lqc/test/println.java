043e372e6b84d30a08cf58c8aaa6156bc4f7ae96
dreamery6 <982464115@qq.com>
2021-01-04 19:56:48 +0800

    hello world

---
+++ test/src/cn/lqc/test/println.java
@@ -0,0 +1,32 @@
+package cn.lqc.test;
+
+public class println {
+	public enum test {
+		hell,
+		hell2,
+		hell3
+	}
+	public enum Gender {
+		男,
+		女,
+		保密
+	}
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 11;
+		int c = a + b;
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("dasdasd");
+		buffer.append("rfesdgrdg");
+
+		System.out.println(Gender.保密);
+		System.out.println("1233");
+		
+		
+	}
+
+	public void test() {
+		String[] arg = new String[6];
+		int[] arr = new int[5];
+	}
+}
